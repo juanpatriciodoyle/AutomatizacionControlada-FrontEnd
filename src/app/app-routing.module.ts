750010f06b5fa23710b29f1b781e21b072c2b811
juan <juandoyle@sirius.com.ar>
2019-08-06 17:25:34 -0300

    boton talba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EmployeeComponent} from './Employees/employee.component';
+import {EmployeeDetailsComponent} from './EmployeesDetails/employeeDetails.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'employees', component: EmployeeComponent},
+  {path: 'employeesDetails', component: EmployeeDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {EmployeeComponent} from './Employees/employee.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {EmployeeDetailsComponent} from './EmployeesDetails/employeeDetails.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './Material/material-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    EmployeeDetailsComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
