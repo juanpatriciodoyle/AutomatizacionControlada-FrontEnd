2689a018986459b46cd09e41d6eb727890e4da2d
juan <juandoyle@sirius.com.ar>
2019-09-18 14:28:08 -0300

    TechnicalService.ts done

--- src/app/TechnicalServices/technicalService.ts
+++ src/app/TechnicalServices/technicalService.ts
@@ -1,52 +1,66 @@
 export class TechnicalService{
   id: number;
-  name: string;
-  surname: string;
-  position: string;
+  employee: number;
+  description: string;
+  admissionDate: Date;
+  egressDate: Date;
+  price: number;
+  paymentMethod: string;
+  delivered: Boolean;
+  status: string;
+
+  //     @OneToOne(cascade = CascadeType.ALL)
+  //     private Employee employee;
+  //     private Enum<Status> status;
+
 
   static from(json: any): TechnicalService {
-    return new TechnicalService(json.id, json.name, json.surname, TechnicalService.enumToSpanish(json.position));
+    return new TechnicalService(json.id, json.employee, json.description, json.admissionDate,
+        json.egressDate, json.price, json.paymentMethod, json.delivered, TechnicalService.enumToSpanish(json.status));
   }
 
   static fromForm(data: any): TechnicalService {
-    return new TechnicalService(data.id, data.name, data.surname, data.position);
+    return new TechnicalService(data.id, data.employee, data.description, data.admissionDate,
+      data.egressDate, data.price, data.paymentMethod, data.delivered, data.status);
   }
 
-  constructor(id: number, name: string, surname: string, position: string) {
+  constructor(id: number, employee: number, description: string, admissionDate: Date, egressDate: Date, price: number, paymentMethod: string, delivered: Boolean, status: string) {
     this.id = id;
-    this.name = name;
-    this.surname = surname;
-    this.position = position;
+    this.employee = employee;
+    this.description = description;
+    this.admissionDate = admissionDate;
+    this.egressDate = egressDate;
+    this.price = price;
+    this.paymentMethod = paymentMethod;
+    this.delivered = delivered;
+    this.status = status;
   }
-  // Enum implementation, service list (add-> lo que retorna lo meto en la lista). Get by id checkeo en la lista primero y sino hago get all
+
+// Enum implementation, service list (add-> lo que retorna lo meto en la lista). Get by id checkeo en la lista primero y sino hago get all
   static enumToSpanish(position: string): string {
     switch (position) {
-      case "BOSS":
-        return "Jefe";
-      case "OWNER":
-        return "Dueño/a";
-      case "TECHNICIAN":
-        return "Técnico";
-      case "SALES":
-        return "Ventas";
+      case "ONSERVICE":
+        return "En servicio";
+      case "DELAYED":
+        return "Retrasada";
+      case "READY":
+        return "Lista";
+      case "DELIVERED":
+        return "Entregada";
     }
   }
 
     static enumToEnglish(position): string {
     debugger;
     switch (position) {
-      case "Jefe":
-        return "BOSS";
-      case "Dueño/a":
-        return "OWNER";
-      case "Técnico":
-        return "TECHNICIAN";
-      case "Ventas":
-        return "SALES";
+      case "En servicio":
+        return "ONSERVICE";
+      case "Retrasada":
+        return "DELAYED";
+      case "Lista":
+        return "READY";
+      case "Entregada":
+        return "DELIVERED";
     }
   }
-}
-
-enum Position {
-  BOSS = "Jefe"
 }
